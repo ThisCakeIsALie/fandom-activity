72815848668b91f2989b6802448f3ec28e042054
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2019-05-31 11:05:46 +0200

    Playing around with material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Typography, IconButton, SwipeableDrawer } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import Container from "@material-ui/core/Container";
+import { Switch, Route } from "react-router-dom";
+import { StartScreen } from "./views/StartScreen";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyle = makeStyles({
+  menuButton: {
+    position: "absolute"
+  },
+  content: {
+    height: "100vh"
+  }
+});
 
 const App: React.FC = () => {
+  const [menuIsOpen, setMenuOpen] = useState(false);
+  const classes = useStyle();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl">
+      <IconButton
+        className={classes.menuButton}
+        onClick={() => setMenuOpen(!menuIsOpen)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <SwipeableDrawer
+        open={menuIsOpen}
+        onOpen={() => setMenuOpen(true)}
+        onClose={() => setMenuOpen(false)}
+      >
+        <Typography>Yeet</Typography>
+      </SwipeableDrawer>
+      <StartScreen className={classes.content} />
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/views/StartScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
+
+
+export const StartScreen: React.FC<{ className?: string }> = props => {
+  return (
+    <>
+      <Grid className={props.className}  justify="center" alignItems="center" direction="column" container>
+        <Typography>Test</Typography>
+        <Typography>Test2</Typography>
+      </Grid>
+    </>
+  );
+};
